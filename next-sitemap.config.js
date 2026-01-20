4aabb14adfe12d790743822574aa408c0819de75
safimiazi <mohibullamiazi@gmail.com>
2026-01-20 23:00:26 +0600

    site map added

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://video-downloader-zeta-ten.vercel.app', // Vercel URL
+  generateRobotsTxt: true, // robots.txt auto generate 
+  sitemapSize: 5000,
+  changefreq: 'daily',
+  priority: 0.7,
+};
